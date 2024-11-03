856c746498d471569f67bb0a50b981acc8cabc21
Luca Spolladore <luca.spolladore@outlook.it>
2024-11-03 17:18:39 +0100

    dependency updates

--- loan_model_api/loan_model_api/settings.py
+++ loan_model_api/loan_model_api/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = 'django-insecure-&o1e41-1x3m(*8!d&e0c^-71psku21#11*xtkap^8yd=-55hof'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
